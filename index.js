e3f7de960277bf42bc54aa392a31f6cc4f17214c
Ezequiel Fidalgo <exekover@gmail.com>
2023-04-12 16:15:55 -0300

    Creacion del Formulario Js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-//Comentario agregado por Javier
+function enviarFormulario() {
+    // Obtener los valores de los campos del formulario
+    const nombreYApellido = document.getElementById('nombreYApellido').value;
+    const telefono = document.getElementById('phone').value;
+    const correoElectronico = document.getElementById('email').value;
+    const asunto = document.getElementById('Asunto').value;
+    const mensaje = document.getElementById('mensaje').value;
+    
+    // Validar que los campos requeridos no estén vacíos
+    if (nombreYApellido === '' || correoElectronico === '' || mensaje === '') {
+      alert('Por favor complete los campos requeridos');
+      return;
+    }
+    
+    // Crear un objeto con los datos del formulario
+    const datosFormulario = {
+      nombreYApellido,
+      telefono,
+      correoElectronico,
+      asunto,
+      mensaje
+    };
+    
+    
+    // Mostrar un mensaje de confirmación después de enviar los datos
+    alert('El formulario se envió correctamente');
+  }
